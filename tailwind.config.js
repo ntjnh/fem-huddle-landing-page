4453191ccc773d01fb97a8aa070d21dfab4d1a6f
Nate J.M <nate@natejonah.com>
2023-11-06 23:45:44 +0000

    Set up postcss and tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{html,js,jsx}"],
+    theme: {
+        fontFamily: {
+            'body': ['Open Sans', 'sans-serif'],
+            'display': ['Poppins', 'sans-serif'],
+        },
+        extend: {},
+    },
+    plugins: [],
+}
