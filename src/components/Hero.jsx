b498030d3981ebc9c385c660184cae701df8c040
Nate J.M <nate@natejonah.com>
2023-11-07 23:54:51 +0000

    Start styling hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,19 @@ export default function Hero() {
     return (
         <section>
             <div className="container mx-auto">
-                <h1>Build The Community Your Fans Will Love</h1>
+                <div className="flex items-center">
+                    <div className="pr-20 w-[46%]">
+                        <h1 className="leading-normal text-4xl text-cyan-975">Build The Community <br />Your Fans Will Love</h1>
 
-                <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
+                        <p className="text-cyan-975">Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
 
-                <a href="#">Get Started For Free</a>
+                        <a className="bg-pink-450 font-normal inline-block mt-8 px-16 py-4 rounded-full text-sm text-white" href="#">Get Started For Free</a>
+                    </div>
 
-                <img className="max-w-[680px]" src={mockupImg} alt="Mockup" />
+                    <div className="w-[54%]">
+                        <img className="max-w-[680px]" src={mockupImg} alt="Mockup" />
+                    </div>
+                </div>
             </div>
         </section>
     )
