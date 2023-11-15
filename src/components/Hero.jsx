6a1f8a5fba9defd71db702cc0ded7d03b182e2ba
Nate J.M <nate@natejonah.com>
2023-11-15 22:28:59 +0000

    Tidy up font and spacing in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,15 +2,15 @@ import mockupImg from '../assets/images/illustration-mockups.svg'
 
 export default function Hero() {
     return (
-        <section>
+        <section className="py-16">
             <div className="container mx-auto">
                 <div className="flex items-center">
                     <div className="pr-20 w-[46%]">
-                        <h1 className="leading-normal text-4xl text-cyan-975">Build The Community <br />Your Fans Will Love</h1>
+                        <h1 className="leading-normal mb-7 text-[40px] text-cyan-975">Build The Community <br />Your Fans Will Love</h1>
 
                         <p className="text-cyan-975">Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
 
-                        <a className="bg-pink-450 font-normal inline-block mt-8 px-16 py-4 rounded-full text-sm text-white" href="#">Get Started For Free</a>
+                        <a className="bg-pink-450 font-bold inline-block mt-8 px-16 py-4 rounded-full text-sm text-white" href="#">Get Started For Free</a>
                     </div>
 
                     <div className="w-[54%]">
