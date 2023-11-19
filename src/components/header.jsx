30a7b1b60bc7d5a8b3375b901aaceb50854767e3
Nate J.M <nate@natejonah.com>
2023-11-19 21:21:27 +0000

    Header component mobile styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,15 +10,15 @@ export default function Header(props) {
     }
 
     return (
-        <header className="bg-cyan-25 pt-12" style={bgStyles}>
+        <header className="bg-cyan-25 pt-5 px-4 md:pt-12 md:px-0" style={bgStyles}>
             <div className="container mx-auto">
                 <div className="flex items-center justify-between">
                     <div className="w-[49%]">
-                        <img className="max-w-[200px]" src={logo} alt="Huddle logo" />
+                        <img className="max-w-[100px] md:max-w-[200px]" src={logo} alt="Huddle logo" />
                     </div>
 
                     <div className="text-right w-[49%]">
-                        <a className="bg-white font-bold inline-block px-16 py-3 rounded-full shadow-[0_7px_10px_-2px_rgba(0,0,0,0.1)] text-sm text-cyan-975" href="#">Try It Free</a>
+                        <a className="bg-white font-bold inline-block px-6 py-1 md:px-16 md:py-3 rounded-full shadow-[0_7px_10px_-2px_rgba(0,0,0,0.1)] text-[10px] md:text-sm text-cyan-975" href="#">Try It Free</a>
                     </div>
                 </div>
             </div>
