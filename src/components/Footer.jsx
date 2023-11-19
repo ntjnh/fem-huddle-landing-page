46ad7b40f50a128abb3432d9d16b4f9a8290d1d9
Nate J.M <nate@natejonah.com>
2023-11-19 23:03:52 +0000

    Start adding style to footer on mob

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,27 +5,27 @@ import twitter from '../assets/images/twitter.svg'
 
 export default function Footer() {
     return (
-        <footer className="bg-cyan-975 text-white">
+        <footer className="bg-cyan-975 pt-32 px-3 md:pt-0 text-base text-cyan-25">
             <div className="container mx-auto">
-                <img src={logo} alt="Huddle" />
+                <img className="mb-6" src={logo} alt="Huddle" />
 
-                <address>
+                <address className="mb-6 not-italic max-w-[295px]">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
                 </address>
 
-                <a href="tel:+15431234567">+1-543-123-4567</a>
-                <a href="mailto:example@huddle.com">example@huddle.com</a>
+                <a className="block mb-6" href="tel:+15431234567">+1-543-123-4567</a>
+                <a className="block mb-16" href="mailto:example@huddle.com">example@huddle.com</a>
 
-                <ul>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">What We Do</a></li>
-                    <li><a href="#">FAQ</a></li>
-                    <li><a href="#">Career</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li><a href="#">Contact Us</a></li>
+                <ul className="mb-14">
+                    <li><a className="inline-block pb-5" href="#">About Us</a></li>
+                    <li><a className="inline-block pb-5" href="#">What We Do</a></li>
+                    <li><a className="inline-block pb-5" href="#">FAQ</a></li>
+                    <li><a className="inline-block pb-5" href="#">Career</a></li>
+                    <li><a className="inline-block pb-5" href="#">Blog</a></li>
+                    <li><a className="inline-block" href="#">Contact Us</a></li>
                 </ul>
 
-                <ul>
+                <ul className="mb-6">
                     <li>
                         <a href="https://facebook.com" target="_blank" rel="noreferrer nofollow">
                             <img className="h-5 w-5" src={facebook} alt="Facebook" />
@@ -43,7 +43,7 @@ export default function Footer() {
                     </li>
                 </ul>
                 
-                <p>&copy; Copyright 2018 Huddle. All rights reserved.</p>
+                <p className="text-center text-xs">&copy; Copyright 2018 Huddle. All rights reserved.</p>
             </div>
 
             <div className="p-4 text-center text-xs">
