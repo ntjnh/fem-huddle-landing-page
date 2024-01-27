b0aa9ff297c240c700c7af85bf72d66c44cbb585
Nate J.M <nate@natejonah.com>
2024-01-27 22:48:03 +0000

    CTA tablet tweaks

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,6 +1,6 @@
 export default function CTA() {
     return (
-        <section className="absolute bg-white card-shadow left-0 mx-3 right-0 px-3 py-10 rounded-2xl z-10">
+        <section className="absolute bg-white card-shadow left-0 mx-3 md:mx-10 lg:mx-0 right-0 px-3 py-10 rounded-2xl z-10">
             <div className="container mx-auto text-center">
                 <h3 className="mb-7">Ready To Build Your Community?</h3>
                 <a className="bg-pink-450 font-bold inline-block px-16 py-3 md:py-4 rounded-full text-[10px] md:text-sm text-white" href="#">Get Started For Free</a>
