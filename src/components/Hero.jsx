c29c2dac97730595aa0a9cd41c058441178b4a71
Nate J.M <nate@natejonah.com>
2024-01-27 22:32:00 +0000

    Amend width class in hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
                     <div className="px-5 md:px-0 lg:pl-5 lg:pr-20 lg:w-[46%]">
                         <h1 className="leading-normal mb-6 lg:mb-7 text-2xl md:text-[40px] text-cyan-975">Build The Community <br />Your Fans Will Love</h1>
 
-                        <p className="text-cyan-975 text-base md:mx-auto md:text-lg md:w-[75%] lg:mx-0 lg:w-auto">Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
+                        <p className="text-cyan-975 text-base md:mx-auto md:text-lg md:w-3/4 lg:mx-0 lg:w-auto">Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
 
                         <a className="bg-pink-450 font-bold inline-block mt-8 px-16 py-3 md:py-4 rounded-full text-xs md:text-sm text-white" href="#">Get Started For Free</a>
                     </div>
